c8ff19ada690d7e829e3d1b0483f6d1f72283313
yanirc1981 <cerzurc@hotmail.com>
2024-10-10 18:05:03 -0300

    url back

--- CodeFront/codeFidelizame/src/Config.js
+++ CodeFront/codeFidelizame/src/Config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:3001';
-
+//export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://codefidelizame.onrender.com';
 
 const Config = {
     BASE_URL,
